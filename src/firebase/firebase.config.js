0d015a2b99b34964313ce796cc3054c4f83d7c79
Orchizakia00 <zakiaislam290@gmail.com>
2023-10-06 22:47:51 +0600

    project setup completed

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEgmPi-7Y2zzu2nHZwIBg91rYnsQFFpe4",
+  authDomain: "evento-ce596.firebaseapp.com",
+  projectId: "evento-ce596",
+  storageBucket: "evento-ce596.appspot.com",
+  messagingSenderId: "781840779882",
+  appId: "1:781840779882:web:dfd9f9c2eea914b7421813"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
