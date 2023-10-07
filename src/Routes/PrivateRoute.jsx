1c381240f3b71e241ff141364b39e6d4f64d231a
Orchizakia00 <zakiaislam290@gmail.com>
2023-10-07 20:04:13 +0600

    spinner edited

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -8,7 +8,7 @@ const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
 
     if (loading) {
-        return <span className="loading loading-spinner text-info loading-lg"></span>
+        return <span className="loading loading-spinner text-info loading-lg flex justify-center items-center mx-auto my-10"></span>
     }
 
     if (!user) {
